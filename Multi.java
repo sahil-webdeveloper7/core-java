a07c86d9171d7576764a1b4901004612af33e839
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-08-19 10:59:44 +0530

    Fourth program

---
+++ Multi.java
@@ -0,0 +1,28 @@
+public class Multi{
+
+
+public static void main(String args[]){
+
+int a = 20;
+int b = 30;
+int sum = a*b;
+
+System.out.print("Sum="+sum);
+
+}
+}
+
+class Div{
+
+
+public static void main(String args[]){
+
+int a = 20;
+int b = 30;
+int sum = a/b;
+
+System.out.print("Sum="+sum);
+
+}
+}
+
