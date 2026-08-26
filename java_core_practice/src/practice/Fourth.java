9a6abc27b10df773ca6caa077c928908e3b79df2
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-08-26 11:29:22 +0530

    binary operation

---
+++ java_core_practice/src/practice/Fourth.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Fourth {
+	public static void main(String[] args) {
+		int num = 20;
+		
+		System.out.println(num + num - num - num + num);
+		System.out.println(num - num - num);
+		
+		
+		
+	}
+
+}
