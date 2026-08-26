0bc3bbeeb4a1868ed1eb6c5e073f7b264fc1b753
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-08-26 11:37:13 +0530

    arithmetic operation

---
+++ java_core_practice/src/practice/Fifth.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Fifth {
+	public static void main(String[] args) {
+		int num = 20;
+		System.out.println(num+5);
+		System.out.println(num-5);
+		System.out.println(num*5);
+		System.out.println(num/5);
+		System.out.println(num%5);
+		System.out.println(num*20);
+	}
+
+}
