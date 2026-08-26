40b346fb69364a306fa4cf27384d4b8db7399f03
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-08-26 11:24:15 +0530

    widing casting

---
+++ java_core_practice/src/practice/Third.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Third {
+
+	public static void main(String[] args) {
+		int num = 15;
+		System.out.println(++num + --num + ++num + ++num);
+		System.out.println(--num + num++ + num--);
+		System.out.println(++num);
+		System.out.println(--num);
+	}
+
+}
