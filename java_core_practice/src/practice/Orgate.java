1c8b79bea6a36b37d77febf14c1712f5d919fc22
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-08-26 12:07:34 +0530

    logical operations

---
+++ java_core_practice/src/practice/Orgate.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Orgate {
+	public static void main(String[] args) {
+		
+		System.out.println(10>100 || 20!=30);
+		System.out.println(101>100 || 20>10);
+		System.out.println(true || false || false || true);
+		System.out.println(10>100 || 20!=30 || 35>= 30);
+		System.out.println(101>100 || 20<5);
+		System.out.println(10>100 || 20<5);
+		System.out.println(true || false || true && false || false);
+		
+		
+
+	}
+
+}
