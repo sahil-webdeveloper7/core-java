df86ba689f347668c623cba0a3dce8cb45b92af8
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-08-26 12:03:10 +0530

    logical operations

---
+++ java_core_practice/src/practice/Andgate.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Andgate {
+
+	public static void main(String[] args) {
+		
+		System.out.println(10>100 && 20!=30);
+		System.out.println(101>100 && 20>10);
+		System.out.println(true && false && false && true);
+		System.out.println(10>100 && 20!=30 && 35>= 30);
+		System.out.println(101>100 && 20<5);
+		
+
+	}
+
+}
