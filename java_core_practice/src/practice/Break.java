47ad34fabd365d32db4be35996f0b4a5014bfce3
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-04 12:16:14 +0530

    Break

---
+++ java_core_practice/src/practice/Break.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Break {
+
+	public static void main(String[] args) {
+		
+		for(int i =1;i<=20;i++) {
+			
+			if(i==18) {
+				break;   // Break:: means it will terminate the program when the number occurs
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
