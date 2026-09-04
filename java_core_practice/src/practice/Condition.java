2e92acc716a4a05892dc8a5b209988e8a86c0ec6
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-04 11:56:04 +0530

    loop

---
+++ java_core_practice/src/practice/Condition.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Condition {
+
+	public static void main(String[] args) {
+		double marks = 80.00;
+		
+		if(marks <=35 && marks>=50)
+		{	
+			System.out.println("C grade");
+		}
+		else if(marks <=51 && marks>=60)
+		{	
+			System.out.println("b grade");
+		}
+
+	}
+
+}
