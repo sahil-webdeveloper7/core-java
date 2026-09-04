d02ceec846b2223f9bc7d86ecf87b743a6ee2e9b
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-04 12:09:38 +0530

    continue

---
+++ java_core_practice/src/practice/Continue.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=15;i++) {
+			if(i==10 ) {
+				continue;  //continue:: means it will skip that number
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
