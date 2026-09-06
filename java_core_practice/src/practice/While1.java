1228da3237e383b84f53b11366e9d5b0b10f1f00
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-06 14:33:39 +0530

    LOOP

---
+++ java_core_practice/src/practice/While1.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class While1 {
+
+	public static void main(String[] args) {
+		int num=1;
+		
+		while(num<=500)
+		{
+			System.out.println(num);
+			num++;
+		}
+
+	}
+
+}
