f86b1a966b374a5c89d5884b886b026215b3cf21
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-07 10:25:02 +0530

    WHILE LOOP

---
+++ java_core_practice/src/practice/Whileloop3.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class Whileloop3 {
+
+	public static void main(String[] args) {
+		int num =1;
+		int counter=0;
+		
+		while(num<=20)
+		{
+			System.out.println("*");
+			num++;
+			counter++;
+		}
+		System.out.println("Counter : "+counter);
+			
+
+
+	}
+
+}
