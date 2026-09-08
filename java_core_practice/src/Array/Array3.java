a79832f20ab0f7de5655e78f51f9c12649c6299a
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-08 12:05:51 +0530

    ARRAY

---
+++ java_core_practice/src/Array/Array3.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class Array3 {
+
+	public static void main(String[] args) {
+		int[] num= new int[5];
+		
+		num[0]=10;
+		num[1]=10;
+		num[2]=10;
+		num[3]=10;
+		num[4]=10;
+	//	num[5]=10;   Exception occurs as we have created array of 5 elements only
+		
+		System.out.println(num[0]);
+		System.out.println(num[1]);
+		System.out.println(num[2]);
+		System.out.println(num[3]);
+		System.out.println(num[4]);
+	//	System.out.println(num[5]);
+		
+
+	}
+
+}
