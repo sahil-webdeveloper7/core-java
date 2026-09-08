78541f7069800d7ea391b307f1c87cf3f1ab84ad
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-08 12:02:07 +0530

    ARRAY

---
+++ java_core_practice/src/Array/Array2.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		char[] arr = new char[6];
+		
+		arr[0] = 'a';
+		arr[1] = 'b';
+		arr[2] = 'd';
+		arr[3] = 'g';
+		arr[4] = 'h';
+		arr[5] = 'e';
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		System.out.println(arr[5]);
+		System.out.println("Length :"+arr.length);
+		
+		
+
+	}
+
+}
