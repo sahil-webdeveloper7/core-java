94439b6c931bef2d7befb631fa49af205ba85ffd
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-08 12:01:11 +0530

    ARRAY

---
+++ java_core_practice/src/Array/Array1.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		
+		arr[0] = 80;
+		arr[1] = 89;
+		arr[2] = 98;
+		arr[3] = 30;
+		arr[4] = 45;
+		
+		System.out.println(arr.length);
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		
+		
+		
+
+	}
+
+}
