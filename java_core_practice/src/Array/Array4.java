c74156d3c850a0664ed47862efd2387bfd5167a5
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-09 11:29:02 +0530

    ARRAY

---
+++ java_core_practice/src/Array/Array4.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class Array4 {
+
+	public static void main(String[] args) {
+		
+		int[] arr= new int[1000];
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i] = 10;
+		}
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
