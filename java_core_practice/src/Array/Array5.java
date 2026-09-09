21f9e8327947679169191e203b5c3cf62b917946
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-09 11:28:15 +0530

    ARRAY

---
+++ java_core_practice/src/Array/Array5.java
@@ -0,0 +1,23 @@
+
+
+package Array;
+
+public class Array5 {
+
+	public static void main(String[] args) {
+		int[] num = new int[1000];
+		
+		for(int i=0;i<num.length;i++)
+		{
+			num[i]=i;
+		}
+		
+		
+		for(int i=0;i<num.length;i++)
+		{
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
