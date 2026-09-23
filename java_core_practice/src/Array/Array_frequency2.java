54e66823b4c6c1c327d447812b1cd9d16f990adc
sahil-webdeveloper7 <sahilmohol7@gmail.com>
2026-09-23 17:28:33 +0530

    Frequency

---
+++ java_core_practice/src/Array/Array_frequency2.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class Array_frequency2 {
+
+	public static void main(String[] args) {
+		
+		int [] arr= {10,2,30,0,9,89,39,0,0,9,0};
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			int counter=0;
+			
+			if(arr[i]==0)
+			{
+				counter++;
+				
+			}
+			System.out.print(arr[i]);
+			
+		}
+		
+		
+
+
+
+	}
+
+}
